51dc04f446ba4d4965631d6c2bccb89254688dd9
Edvardas Vabuolas <edvardasvabuolas@Y9YV9MH9CT.local>
2023-01-28 00:08:40 +0000

    New data

--- main.R
+++ main.R
@@ -1,7 +1,10 @@
 ######### packages ###############
 
 ######### Data import ############
-uk_data <- read.csv(file = 'economic_outlook.csv')
+# OECD does not let download all data in a single file, so we download two files and merge them
+uk_data1 <- read.csv(file = 'economic_outlook1.csv', head = T)
+uk_data2 <- read.csv(file = 'economic_outlook2.csv', head = T)
+uk_data <- merge(uk_data1, uk_data2, all = TRUE)
 
 ##### Creating a data frame ######
 # OECD CSV export returns variable names as rows, we need them as columns
@@ -22,28 +25,32 @@ for (i in 1:nrow(uk_data)) {
   }
 }
 
-# Initiate data frame with 165 rows (1979-2019 has 165 quarters)
-df <- as.data.frame(matrix(0, nrow = 165, ncol = 0))
-
+# Initiate data frame with 165 rows (1979Q1-2019Q4 has 165 quarters)
+df <- as.data.frame(matrix(0, nrow = 166, ncol = 0))
 # Iterate over dictionary keys
 for (i in ls(vars_vals)) {
   # Ignore variable if there are missing values
-  if (length(vars_vals[[i]]) == 165) {
+  if (length(vars_vals[[i]]) == 166) {
     # Append data frame with a new column "i" with it's associated values as rows
     df[[i]] <- vars_vals[[i]]
   } else {
-    print(paste("Variable with missing values: ", i))
+    print(paste(
+      "Variable with missing values: ",
+      i,
+      " No. of values: ",
+      length(vars_vals[[i]])
+    ))
   }
 }
 
 
-# "Variable with missing values:  CPIH"       Data starts from 1990
-# "Variable with missing values:  CPIH_YTYPCT"      Data starts from 1991
-# "Variable with missing values:  EXCHER"       Data starts from 2007
-# "Variable with missing values:  PCOREH"       Data starts from 1996
-# "Variable with missing values:  PCOREH_YTYPCT"      Data starts from 1997
-# "Variable with missing values:  PMGSX"      Data starts from 1998
-# "Variable with missing values:  PMNW"       Data starts from 1998
-# "Variable with missing values:  PXGSX"      Data starts from 1998
-# "Variable with missing values:  PXNW"       Data starts from 1998
-# "Variable with missing values:  SHTGSVD"      Data starts from 1980
+# "Variable with missing values:  CPIH"       Data starts from 1990: No. of values:  122
+# "Variable with missing values:  CPIH_YTYPCT"      Data starts from 1991: No. of values:  118
+# "Variable with missing values:  EXCHER"       Data starts from 2007: No. of values:  54
+# "Variable with missing values:  PCOREH"       Data starts from 1996: No. of values:  98
+# "Variable with missing values:  PCOREH_YTYPCT"      Data starts from 1997: No. of values:  94
+# "Variable with missing values:  PMGSX"      Data starts from 1998: No. of values:  130
+# "Variable with missing values:  PMNW"       Data starts from 1998: No. of values:  130
+# "Variable with missing values:  PXGSX"      Data starts from 1998: No. of values:  130
+# "Variable with missing values:  PXNW"       Data starts from 1998: No. of values:  130
+# "Variable with missing values:  SHTGSVD"      Data starts from 1980: No. of values:  162
