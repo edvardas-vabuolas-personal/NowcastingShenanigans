fec813c81a79fd21f591ea9b5eb96483c8adba37
Edvardas Vabuolas <edvardasvabuolas@Y9YV9MH9CT.local>
2023-02-27 22:42:20 +0000

    Separated VAR variables, refactored code

---
+++ load_data.R
@@ -0,0 +1,66 @@
+source('process_data.R')
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(ggplot2)
+
+load_data <- function() {
+  ######### Data import ############
+  # Data used: OECD Economic Outlook for the UK, 1979Q1-2020Q1
+  # https://stats.oecd.org/index.aspx?queryid=51396 (unfiltered)
+  
+  # OECD does not let download all data in a single file, so we download two files and merge them
+  detailed_economic_outlook <- merge(
+    read.csv(file = './oecd_data/economic_outlook1.csv', head = T),
+    read.csv(file = './oecd_data/economic_outlook2.csv', head = T),
+    all = TRUE
+  )
+  
+  detailed_monetary_data <- read.csv('./oecd_data/monetary_stats.csv', head = T)
+  
+  detailed_unemployment <- read.csv('./oecd_data/unemployment_data.csv', head = T)
+  
+  detailed_monthly_inflation <- read.csv('./oecd_data/monthly_inflation.csv', head = T)
+  
+  detailed_production_net_export <- read.csv('./oecd_data/production_net_export.csv', head = T)
+  
+  ready_economic_outlook <- process_data(detailed_economic_outlook, 166, 'VARIABLE')
+  
+  ready_monetary_data <- process_data(detailed_monetary_data, 481, 'Subject')
+  
+  ready_short_term_interest_rates <- process_data(filter(detailed_monetary_data, SUBJECT=='IR3TIB'), 397, 'Subject')
+  
+  ready_unemployment_data <- process_data(detailed_unemployment, 433, 'SUBJECT')
+  
+  ready_monthly_inflation <- process_data(detailed_monthly_inflation, 481, 'Subject')
+  
+  ready_production_net_export <- process_data(detailed_production_net_export, 481, 'Subject')
+  
+  ready_economic_outlook$Time <-
+    str_replace(ready_economic_outlook$Time, 'Q1', '03')
+  ready_economic_outlook$Time <-
+    str_replace(ready_economic_outlook$Time, 'Q2', '06')
+  ready_economic_outlook$Time <-
+    str_replace(ready_economic_outlook$Time, 'Q3', '09')
+  ready_economic_outlook$Time <-
+    str_replace(ready_economic_outlook$Time, 'Q4', '12')
+  
+  ready_datasets <- list(
+    ready_monetary_data,
+    ready_economic_outlook,
+    ready_unemployment_data,
+    ready_monthly_inflation,
+    ready_production_net_export,
+    ready_short_term_interest_rates
+  )
+  
+  monthly_dataset <- 
+    ready_datasets %>% reduce(full_join, by = 'Time')
+  quarterly_dataset <-
+    ready_datasets %>% reduce(inner_join, by = 'Time')
+
+  data = list()
+  data[['monthly']] <- monthly_dataset; data[['quarterly']] <- quarterly_dataset
+  
+  return (data)
+}
--- main.R
+++ main.R
@@ -1,57 +1,20 @@
-#TestNew
-source('process_data.R')
-library(dplyr)
-library(tidyverse)
-library(stringr)
-library(ggplot2)
+source("load_data.R")
 
-######### Data import ############
-# Data used: OECD Economic Outlook for the UK, 1979Q1-2020Q1
-# https://stats.oecd.org/index.aspx?queryid=51396 (unfiltered)
+data <- load_data();
+monthly_dataset = data[['monthly']]; quarterly_dataset = data[['quarterly']];
 
-# OECD does not let download all data in a single file, so we download two files and merge them
-detailed_economic_outlook <- merge(
-  read.csv(file = './oecd_data/economic_outlook1.csv', head = T),
-  read.csv(file = './oecd_data/economic_outlook2.csv', head = T),
-  all = TRUE
-)
+VAR_variables <- monthly_dataset %>% 
+  select(c("Time", "GDP_ANNPCT", "Relative consumer price indices","Short-term interest rates, Per cent per annum", "LRHUTTTT", "Share Prices, Index"))
 
-detailed_monetary_data <- read.csv('./oecd_data/monetary_stats.csv', head = T)
+VAR_table <- as_tibble(VAR_variables) %>% 
+  rename(
+    time = Time,
+    GDP_Growth = GDP_ANNPCT,
+    Inflation = 'Relative consumer price indices',
+    Interest_rate = 'Short-term interest rates, Per cent per annum',
+    Unemployment_Rate = LRHUTTTT,
+    Stock_market_index = 'Share Prices, Index'
+  )
 
-detailed_unemployment <- read.csv('./oecd_data/unemployment_data.csv', head = T)
-
-detailed_monthly_inflation <- read.csv('./oecd_data/monthly_inflation.csv', head = T)
-
-detailed_production_net_export <- read.csv('./oecd_data/production_net_export.csv', head = T)
-
-ready_economic_outlook <- process_data(detailed_economic_outlook, 166, 'Variable')
-
-ready_monetary_data <- process_data(detailed_monetary_data, 481, 'Subject')
-
-ready_unemployment_data <- process_data(detailed_unemployment, 433, 'Subject')
-
-ready_monthly_inflation <- process_data(detailed_monthly_inflation, 481, 'Subject')
-
-ready_production_net_export <- process_data(detailed_production_net_export, 481, 'Subject')
-
-ready_economic_outlook$Time <-
-  str_replace(ready_economic_outlook$Time, 'Q1', '03')
-ready_economic_outlook$Time <-
-  str_replace(ready_economic_outlook$Time, 'Q2', '06')
-ready_economic_outlook$Time <-
-  str_replace(ready_economic_outlook$Time, 'Q3', '09')
-ready_economic_outlook$Time <-
-  str_replace(ready_economic_outlook$Time, 'Q4', '12')
-
-ready_datasets <- list(
-  ready_economic_outlook,
-  ready_monetary_data,
-  ready_unemployment_data,
-  ready_monthly_inflation,
-  ready_production_net_export
-)
-
-monthly_dataset <- 
-  ready_datasets %>% reduce(full_join, by = 'Time')
-quarterly_dataset <-
-  ready_datasets %>% reduce(inner_join, by = 'Time')
+AR_table <- VAR_table %>%
+  select(c(time, GDP_Growth))
