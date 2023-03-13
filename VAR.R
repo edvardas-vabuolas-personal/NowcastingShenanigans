336e067bec95ef31b87db63899ffba98b9f2ada6
Edvardas Vabuolas <edvardasvabuolas@Y9YV9MH9CT.broadband>
2023-03-13 20:17:24 +0000

    Added comments and explanations

--- VAR.R
+++ VAR.R
@@ -1,9 +1,9 @@
 ####### Load Packages ##########
 # library(dplyr) #data manipulation
-library(tidyverse) #data manipulation 
+library(tidyverse) #data manipulation
 library(tidyr) #data manipulation
 library(ggplot2) #data visualisation
-library(caret) #ML training 
+library(caret) #ML training
 library(forecast) #time series forecasting, stationarity testing
 library(tseries)
 library(readxl) #data import
@@ -13,65 +13,154 @@ library(gapminder)
 library(xts)
 library(vars)
 
-nowcasting_dataset <- read_excel("230312 Nowcasting Dataset.xls", 
-                                         col_types = c("numeric", "date", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric", 
-                                                       "numeric", "numeric", "numeric"))
+###### Load Data ########
+nowcasting_dataset <- read_excel(
+  "230312 Nowcasting Dataset.xls",
+  col_types = c(
+    "numeric",
+    "date",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric",
+    "numeric"
+  )
+)
 rownames(nowcasting_dataset) <- nowcasting_dataset$Date
 
-nowcasting_dataset$UK_GDP_4 <- na.approx(nowcasting_dataset$UK_GDP_4)
+###### Process data ######
 
-train <- subset(nowcasting_dataset[, c(3, 5, 12, 20, 37)], subset = nowcasting_dataset$Date <= '2010-12-01')
-test <- subset(nowcasting_dataset[, c(3, 5, 12, 20, 37)], subset = nowcasting_dataset$Date >= '2011-01-01')
+# Linearly interpolate GDP values
+nowcasting_dataset$UK_GDP_4 <-
+  na.approx(nowcasting_dataset$UK_GDP_4)
 
-# VARselect(train, lag.max = 10, type = 'const')
+# Split dataset to train and test sub samples
+train <-
+  subset(nowcasting_dataset[, c(3, 5, 12, 20, 37)], subset = nowcasting_dataset$Date <= '2010-12-01')
+test <-
+  subset(nowcasting_dataset[, c(3, 5, 12, 20, 37)], subset = nowcasting_dataset$Date >= '2011-01-01')
 
+# The output of VARselect tells us what lag length we should use
+VARselect(train, lag.max = 10, type = 'const')
+
+###### One step ahead forecast of test sub sample  ######
+
+# Initiate an empty list for predictions
 list_of_predictions <- list()
+
+# For each row in the test sub sample
 for (i in 1:nrow(test)) {
-  # create a new time series object with the updated data
-  temp_model <- VAR(train, p = 1, type='const')
-  one_step_ahead_forecast <- predict(temp_model, test[i, ], n.ahead = 1)
-  train[nrow(train) + 1, ] = as.list(test[i,])
-  list_of_predictions <- append(list_of_predictions, one_step_ahead_forecast$fcst$UK_GDP_4[, 1])
+  # Obtain coefficients for VAR(1) lag length
+  temp_model <- VAR(train, p = 1, type = 'const')
+  
+  # Forecast one step ahead; feed one observation from test sub sample
+  one_step_ahead_forecast <-
+    predict(temp_model, test[i,], n.ahead = 1)
+  
+  # Append train sub sample with one observatin from the test sub sample
+  train[nrow(train) + 1,] = as.list(test[i, ])
+  
+  # Append the list of predictins with the one ahead forecast
+  list_of_predictions <-
+    append(list_of_predictions,
+           one_step_ahead_forecast$fcst$UK_GDP_4[, 1])
 }
 
-msfe <- sum((as.numeric(list_of_predictions) - test$UK_GDP_4)^2) / nrow(test)
+# Calculate MSFE. SUM(residuals^2) / N
+msfe <-
+  sum((as.numeric(list_of_predictions) - test$UK_GDP_4) ^ 2) / nrow(test)
 
-dates_for_plot <- seq(as.Date("2011-01-01"), as.Date("2018-03-01"), by="month")
+##### Plot predictions and observations #####
 
+# Initiate an array of monthly dates from 2011 to 2018
+dates_for_plot <-
+  seq(as.Date("2011-01-01"), as.Date("2018-03-01"), by = "month")
+
+# Put predictions and an array of dates into a dataframe
 predictions_df <- data.frame(list_of_predictions, dates_for_plot)
 
-### Plot predictions and observations ###
-colors <- c("Predictions" = "dark green", "Observations" = "steelblue")
+# Color selection
+colors <-
+  c("Predictions" = "dark green",
+    "Observations" = "steelblue")
+
+# Plot
 ggplot() +
-  # Draw line
-  geom_line(data = predictions_df, 
-            aes(
-              x = as.Date(dates_for_plot),
-              y = as.numeric(list_of_predictions), 
-              color = "Predictions"),
-             size = 1) +
   
-  geom_line(data = test[,1], aes(x = as.Date(dates_for_plot),
-                         y = test$UK_GDP_4, color = "Observations"), 
-             size = 1) +
-  # Change x axis title
+  # Draw predictions line
+  geom_line(
+    data = predictions_df,
+    aes(
+      x = as.Date(dates_for_plot),
+      y = as.numeric(list_of_predictions),
+      color = "Predictions"
+    ),
+    size = 1
+  ) +
+  
+  # Draw observations line
+  geom_line(
+    data = test[, 1],
+    aes(
+      x = as.Date(dates_for_plot),
+      y = test$UK_GDP_4,
+      color = "Observations"
+    ),
+    size = 1
+  ) +
+  
+  # Change x and y titles
   labs(x = "Forecast Date", y = "GDP Growth", color = "Legend") +
+  
   # Set x breaks and the desired format for the date labels
   scale_x_date(date_breaks = "2 months", date_labels = "%m-%Y") +
-  scale_color_manual(values = colors) + 
+  
+  # Apply colours
+  scale_color_manual(values = colors) +
+  
+  # Rotate x axis labels by 45 degrees
   theme(axis.text.x = element_text(angle = 45))
